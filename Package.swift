52c145a43ef4817d8ee9c342b51bb54e4657bb4b
Васин Валерий Михайлович <vasinvm@akbarsdigital.ru>
2025-01-27 19:45:57 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,28 +1,15 @@
-// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
-    name: "CardIOFork",
-    platforms: [
-        .iOS(.v11)
-    ],
-    products: [
-        // library (для статической/динамической линковки)
-        .library(
-            name: "CardIOFork",
-            targets: ["CardIOFork"]
-        )
-    ],
-    targets: [
-        // Основной таргет, где лежит исходный код библиотеки
-        .target(
-            name: "CardIOFork",
-            path: "CardIO",
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("path/to/headers") 
-            ]
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+        name: "CardIO",
+        products: [
+                .library(
+                        name: "CardIO",
+                        targets: ["CardIO"]),
+        ],
+        dependencies: [
+        ],
+        targets: [
+                .binaryTarget(name: "CardIO", path: "./Sources/CardIO.xcframework")
+        ]
 )
